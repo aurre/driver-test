ec19815125abb226917a67a6b2e409cd43300a62
raysa <aurrecochearaysa@gmail.com>
2018-10-07 18:51:20 -0500

    add functionality to question card

---
+++ client/components/success-alert.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import ErrorIcon from '@material-ui/icons/Error';
+import green from '@material-ui/core/colors/green';
+import IconButton from '@material-ui/core/IconButton';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import { withStyles } from '@material-ui/core/styles';
+
+const variantIcon = {
+  success: CheckCircleIcon,
+  error: ErrorIcon,
+};
+
+const styles1 = theme => ({
+  success: {
+    backgroundColor: green[600],
+  },
+  error: {
+    backgroundColor: theme.palette.error.dark,
+  },
+  icon: {
+    fontSize: 20,
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing.unit,
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+});
+
+function MySnackbarContent(props) {
+  const { classes, className, message, variant } = props;
+  const Icon = variantIcon[variant];
+
+  return (
+    <SnackbarContent
+      className={classNames(classes[variant], className)}
+      aria-describedby="client-snackbar"
+      message={
+        <span id="client-snackbar" className={classes.message}>
+          <Icon className={classNames(classes.icon, classes.iconVariant)} />
+          {message}
+        </span>
+      }
+      action={[
+        <IconButton
+          key="close"
+          aria-label="Close"
+          color="inherit"
+          className={classes.close}
+        />,
+      ]}
+    />
+  );
+}
+
+MySnackbarContent.propTypes = {
+  classes: PropTypes.object.isRequired,
+  className: PropTypes.string,
+  message: PropTypes.node,
+  variant: PropTypes.oneOf(['success', 'error']).isRequired,
+};
+
+const MySnackbarContentWrapper = withStyles(styles1)(MySnackbarContent);
+
+const styles2 = theme => ({
+  margin: {
+    margin: theme.spacing.unit,
+  },
+  witdh: {
+    witdh: 20,
+  },
+});
+
+class CustomizedSnackbars extends React.Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.witdh}>
+        <MySnackbarContentWrapper
+          variant="success"
+          className={classes.margin}
+          message="Well Done!"
+        />
+      </div>
+    );
+  }
+}
+
+CustomizedSnackbars.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles2)(CustomizedSnackbars);
--- client/components/test-questions.js
+++ client/components/test-questions.js
@@ -1,40 +1,87 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { PropType } from 'prop-types';
 import './test-questions.css';
 import { getQuestionThunk } from '../store/question';
+import SuccessAlert from './success-alert';
+import ErrorAlert from './error-alert';
 
 import Checkbox from '@material-ui/core/Checkbox';
 import ListItem from '@material-ui/core/ListItem';
 import List from '@material-ui/core/List';
 import ListItemText from '@material-ui/core/ListItemText';
 import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
 
 class Question extends React.Component {
   constructor() {
     super();
     this.state = {
       IdCounter: 1,
+      checked: [],
+      correct: false,
+      failed: false,
+      correctAnswer: '',
     };
   }
 
-  async componentDidMount() {
-    console.log(this.props.question);
+  handleNext = async () => {
+    this.setState({ correct: false, failed: false });
     await this.props.getQuestionThunk(this.state.IdCounter);
     this.setState(prevState => {
-      return { IdCounter: prevState.IdCounter++ };
+      return { IdCounter: prevState.IdCounter + 1 };
+    });
+  };
+
+  handleSubmitAnswer = () => {
+    let answers = this.props.question.question.answers;
+    answers.forEach(answer => {
+      if (answer.correct) {
+        this.setState({ correctAnswer: answer.answer });
+      }
+    });
+    answers.forEach(answer => {
+      if (this.state.checked.indexOf(answer.id) !== -1) {
+        if (answer.correct) {
+          this.setState({ correct: true });
+        } else {
+          this.setState({ failed: true, checked: [] });
+        }
+      }
+    });
+  };
+
+  handleToggle = value => () => {
+    const { checked } = this.state;
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    this.setState({
+      checked: newChecked,
+    });
+  };
+
+  async componentDidMount() {
+    await this.props.getQuestionThunk(this.state.IdCounter);
+    this.setState(prevState => {
+      return { IdCounter: prevState.IdCounter + 1 };
     });
   }
 
   render() {
-    console.log(this.props.question.question.question);
-    console.log(this.props.question.question.answers);
-
-    let question = this.props.question.question.question;
+    let question = this.props.question.question.question || '';
     let answers = this.props.question.question.answers || [];
     return (
       <Card className="bg">
-        <h4 className="center">{question || 'Question'}</h4>
+        <h4 className="center">{question}</h4>
+        {this.state.correct && <SuccessAlert />}
+        {this.state.failed && <ErrorAlert props={this.state.correctAnswer} />}
+
         <div className="root">
           <List>
             {answers.map(value => (
@@ -43,11 +90,10 @@ class Question extends React.Component {
                 role={undefined}
                 dense
                 button
-                //   onClick={this.handleToggle(value.id)}
-                // className={classes.listItem}
+                onClick={this.handleToggle(value.id)}
               >
                 <Checkbox
-                  // checked={this.state.checked.indexOf(value.id) !== -1}
+                  checked={this.state.checked.indexOf(value.id) !== -1}
                   tabIndex={-1}
                   disableRipple
                 />
@@ -55,6 +101,22 @@ class Question extends React.Component {
               </ListItem>
             ))}
           </List>
+        </div>
+        <div className="buttons-container">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={this.handleSubmitAnswer}
+          >
+            Submit
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={this.handleNext}
+          >
+            Next
+          </Button>
         </div>
       </Card>
     );
